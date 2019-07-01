af86fc918fc0afafe9d6fec569d9c7724bf36607
Arseniy Obolenskiy <gooddoog@student.su>
2019-07-01 21:55:42 +0300

    Window hotfix

--- app/fishing-time.cpp
+++ app/fishing-time.cpp
@@ -5,12 +5,10 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    if (argc < 2) {
-        MainWindow window(nullptr, "config.txt");
-        window.show();
-    } else {
-        MainWindow window(nullptr, argv[1]);
-        window.show();
-    }
+    QString config = "config.txt";
+    if (argc >= 2)
+        config = argv[1];
+    MainWindow window(nullptr, config);
+    window.show();
     return app.exec();
 }
