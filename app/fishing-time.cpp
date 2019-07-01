a6bbbb779c9338d9afdfcdd14e185b019c64dab1
Arseniy Obolenskiy <gooddoog@student.su>
2019-07-01 21:33:58 +0300

    (#1) Add configuration file specification

--- app/fishing-time.cpp
+++ app/fishing-time.cpp
@@ -1,9 +1,15 @@
 #include <QApplication>
+#include <QDebug>
 #include "mainwindow.h"
+#include "user.h"
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        qDebug() << "Please, specify configuration file path";
+        exit(1);
+    }
     QApplication app(argc, argv);
-    MainWindow window;
+    MainWindow window(nullptr, argv[1]);
     window.show();
     return app.exec();
 }
