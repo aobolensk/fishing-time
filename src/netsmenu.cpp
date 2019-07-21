266e450e936dfe4a3412d6c992f228226030b312
Arseniy Obolenskiy <gooddoog@student.su>
2019-07-21 22:51:11 +0300

    (#28) Add ability to add item to the net slot

--- src/netsmenu.cpp
+++ src/netsmenu.cpp
@@ -15,6 +15,27 @@ NetsMenu::NetsMenu(Game *game, QGridLayout *grid) :
         grid->addWidget(&netSlot[i], 2 + (i / 3) * 2, i % 3);
         netSlot[i].setVisible(false);
         netSlot[i].setEnabled(false);
+        #define X(i) \
+        connect(&netSlot[i], static_cast<void(QComboBox::*)(int)>(&QComboBox::currentIndexChanged), \
+            [this](int index) { \
+                if (index == 0) \
+                    return; \
+                if (nets[i] != "") { \
+                    this->game->users[this->game->activeUser].inventory.changeItem(nets[i], 1); \
+                } \
+                if (index == 1) { \
+                    nets[i] = ""; \
+                } else { \
+                    nets[i] = netSlot[i].itemText(index).split(' ').first(); \
+                    qDebug() << netSlot[i].itemText(index).split(' ').first(); \
+                    this->game->users[this->game->activeUser].inventory.changeItem(nets[i], -1); \
+                } \
+                netSlot[i].setCurrentIndex(0); \
+            });
+        X(0); X(1); X(2);
+        X(3); X(4); X(5);
+        X(6); X(7); X(8);
+        #undef X
     }
 
     grid->addWidget(&backButton, 3 + SLOTS_COUNT / 3 * 2, 1);
@@ -27,10 +48,17 @@ void NetsMenu::updateNets() {
     const QMap<QString, int> &inv = game->users[game->activeUser].inventory.get();
     auto it = inv.begin();
     for (int i = 0; i < SLOTS_COUNT; ++i) {
+        netSlotLabel[i].setText(game->str.netSlot.arg(
+            QString::number(i),
+            nets[i] == "" ? game->str.empty : nets[i]
+        ));
+
+        netSlot[i].clear();
+        netSlot[i].addItem("");
         netSlot[i].addItem(game->str.empty);
     }
     while (it != inv.end()) {
-        if (QRegExp("net.*").exactMatch(it.key())) {
+        if (QRegExp("fish.*").exactMatch(it.key())) {
             for (int i = 0; i < SLOTS_COUNT; ++i) {
                 netSlot[i].addItem(it.key() + " (" + QString::number(it.value()) + ')');
             }
@@ -46,10 +74,6 @@ void NetsMenu::display() {
     netsDescription.setVisible(true);
 
     for (int i = 0; i < SLOTS_COUNT; ++i) {
-        netSlotLabel[i].setText(game->str.netSlot.arg(
-            QString::number(i),
-            nets[i] == "" ? game->str.empty : nets[i]
-        ));
         netSlotLabel[i].setVisible(true);
 
         netSlot[i].setVisible(true);
@@ -74,6 +98,7 @@ void NetsMenu::hide() {
 
         netSlot[i].setVisible(false);
         netSlot[i].setEnabled(false);
+        disconnect(&netSlot[i]);
     }
 
     backButton.setVisible(false);
