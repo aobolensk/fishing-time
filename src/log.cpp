789fb85c7310389110a1713347f352f97785625e
Arseniy Obolenskiy <gooddoog@student.su>
2019-09-03 23:41:56 +0300

    (#161) Attempt to fix strange output in console

--- src/log.cpp
+++ src/log.cpp
@@ -6,29 +6,36 @@ Log::Log(Game *game, QTextEdit *logField) :
     console(logField) {}
 
 void Log::info(const QString &message) {
+    console->moveCursor(QTextCursor::End);
     console->insertHtml(game->str.logInfo + ' ' + message + "<br>");
 }
 
 void Log::warning(const QString &message) {
+    console->moveCursor(QTextCursor::End);
     console->insertHtml("<font color=\"orange\">" + game->str.logWarning + ' ' + message + "</font><br>");
 }
 
 void Log::error(const QString &message) {
+    console->moveCursor(QTextCursor::End);
     console->insertHtml("<font color=\"red\">" + game->str.logError + ' ' + message + "</font><br>");
 }
 
 void Log::write(const QString &message) {
+    console->moveCursor(QTextCursor::End);
     console->insertHtml(message + ' ');
 }
 
 void Log::writeln(const QString &message) {
+    console->moveCursor(QTextCursor::End);
     console->insertHtml(message + "<br>");
 }
 
 void Log::setColor(const QString &color) {
+    console->moveCursor(QTextCursor::End);
     console->insertHtml(QString("<font color=\"%1\">").arg(color));
 }
 
 void Log::resetColor() {
+    console->moveCursor(QTextCursor::End);
     console->insertHtml("</font>");
 }
