e144dfae7e404670218e819eeac3103df9a0f1b2
Arseniy Obolenskiy <gooddoog@student.su>
2019-11-15 12:49:20 +0300

    (#332) Implement FileLog

---
+++ include/filelog.h
@@ -0,0 +1,28 @@
+#ifndef INCLUDE_FILELOG_H_
+#define INCLUDE_FILELOG_H_
+#include <QFile>
+#include <QTextStream>
+#include "log.h"
+
+class FileLog : public Log {
+private:
+    QFile file;
+    QTextStream *fs;
+public:
+    virtual void debug(const QString &message) override;
+    virtual void info(const QString &message) override;
+    virtual void warning(const QString &message) override;
+    virtual void error(const QString &message) override;
+    virtual void debugT(const QString &message) override;
+    virtual void infoT(const QString &message) override;
+    virtual void warningT(const QString &message) override;
+    virtual void errorT(const QString &message) override;
+    virtual void write(const QString &message) override;
+    virtual void writeln(const QString &message) override;
+    virtual void setColor(const QString &color) override;
+    virtual void resetColor() override;
+    FileLog(Game *game, const QString &filePath);
+    ~FileLog();
+};
+
+#endif  // INCLUDE_FILELOG_H_
---
+++ src/filelog.cpp
@@ -0,0 +1,64 @@
+#include "filelog.h"
+#include "game.h"
+
+FileLog::FileLog(Game *game, const QString &filePath) :
+    Log(game),
+    file(QFile(filePath)),
+    fs(nullptr) {
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Append)) {
+        qDebug() << "Failed to open file '" << filePath << "' for logging";
+    }
+    fs = new QTextStream(&file);
+}
+
+void FileLog::debug(const QString &message) {
+    *fs << game->str.logDebug << ' ' << message << '\n';
+}
+
+void FileLog::info(const QString &message) {
+    *fs << game->str.logInfo << ' ' << message << '\n';
+}
+
+void FileLog::warning(const QString &message) {
+    *fs << game->str.logWarning << ' ' << message << '\n';
+}
+
+void FileLog::error(const QString &message) {
+    *fs << game->str.logError << ' ' << message << '\n';
+}
+
+void FileLog::debugT(const QString &message) {
+    *fs << getTimestamp() << ' ' << game->str.logDebug << ' ' << message << '\n';
+}
+
+void FileLog::infoT(const QString &message) {
+    *fs << getTimestamp() << ' ' << game->str.logInfo << ' ' << message << '\n';
+}
+
+void FileLog::warningT(const QString &message) {
+    *fs << getTimestamp() << ' ' << game->str.logWarning << ' ' << message << '\n';
+}
+
+void FileLog::errorT(const QString &message) {
+    *fs << getTimestamp() << ' ' << game->str.logError << ' ' << message << '\n';
+}
+
+void FileLog::write(const QString &message) {
+    *fs << message << ' ';
+}
+
+void FileLog::writeln(const QString &message) {
+    *fs << message << '\n';
+}
+
+void FileLog::setColor(const QString &color) {
+    (void)color;
+}
+
+void FileLog::resetColor() {
+
+}
+
+FileLog::~FileLog() {
+    delete fs;
+}
