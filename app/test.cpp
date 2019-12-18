c449046af7cdbe83965c9dff0d5deee2c93c6b5b
Arseniy Obolenskiy <gooddoog@student.su>
2019-12-18 10:57:43 +0300

    (#414) Print system and compiler info before testing

--- app/test.cpp
+++ app/test.cpp
@@ -1,9 +1,11 @@
+#include "aboutmenu.h"
 #include "testuser.h"
 
 int main(int argc, char *argv[]) {
     (void) argc;
     (void) argv;
-
+    qInfo() << "OS:" << AboutMenu::getSystemInfo();
+    qInfo() << "Compiler:" << AboutMenu::getCompilerInfo();
     int returnCode = 0;
     returnCode |= QTest::qExec(new TestUser, argc, argv);
     return returnCode;
