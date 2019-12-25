a8cbe441d39b7f94fd6f9c810b85127762d2036b
Arseniy Obolenskiy <gooddoog@student.su>
2019-12-25 23:45:12 +0300

    (#433) Change style of error widget

--- src/errorwidget.cpp
+++ src/errorwidget.cpp
@@ -2,8 +2,14 @@
 
 ErrorWidget::ErrorWidget() :
         grid(QGridLayout(this)) {
+    this->setGeometry(100, 200, 640, 480);
+    this->setFixedSize(this->width(), this->height());
     grid.addWidget(&errorLabel, 0, 0);
+    QFont font = errorLabel.font();
+    font.setPointSize(14);
+    errorLabel.setFont(font);
     errorLabel.setText("An unknown error occured!");
+    errorLabel.setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
 }
 
 ErrorWidget::~ErrorWidget() {
