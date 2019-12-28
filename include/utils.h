0cddbb5b7c8431277836fdf51a96f1295fab49fa
Arseniy Obolenskiy <gooddoog@student.su>
2019-12-28 16:29:18 +0300

    (#446) Add global error state

--- include/utils.h
+++ include/utils.h
@@ -1,14 +1,22 @@
 #ifndef INCLUDE_UTILS_H_
 #define INCLUDE_UTILS_H_
-#include <QDebug>
+#include <QApplication>
+#include "errorwidget.h"
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 
-#define FT_ASSERT(expr, text)   \
-    if (!(expr)) {              \
-        qCritical() << text;    \
-        exit(1);                \
-    }                           \
+namespace FT_ERROR {
+    extern ErrorWidget *error;
+}
+
+#define FT_ASSERT(expr, text)                               \
+    if (!(expr)) {                                          \
+        FT_ERROR::error = new ErrorWidget();                \
+        FT_ERROR::error->setErrorText(                      \
+            QStringLiteral("Assertion failed at "           \
+            __FILE__ ":" TOSTRING(__LINE__) ": ") + text);  \
+        qApp->exit(1);                                      \
+    }                                                       \
 
 #endif  // INCLUDE_UTILS_H_
---
+++ src/utils.cpp
@@ -0,0 +1,3 @@
+#include "utils.h"
+
+ErrorWidget *FT_ERROR::error = nullptr;
