3cc6d56ecaeb13894839d6a778ff486964351008
Arseniy Obolenskiy <gooddoog@student.su>
2020-02-02 14:25:00 +0300

    (#398) Get rid of table widget in store menu

--- src/storemenu.cpp
+++ src/storemenu.cpp
@@ -4,44 +4,12 @@
 #include "game.h"
 
 StoreMenu::StoreMenu(Game *game, QGridLayout *grid) :
-        Menu(game, grid),
-        storeTable(game) {
-    grid->addWidget(&selectedItemLabel, 1, 2);
-    selectedItemLabel.setWordWrap(true);
-    selectedItemLabel.setVisible(false);
-    selectedItemLabel.setEnabled(false);
-    selectedItemLabel.setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-
-    grid->addWidget(&infoLabel, 2, 2);
+        Menu(game, grid) {
+    grid->addWidget(&infoLabel, 1, 2);
     infoLabel.setWordWrap(true);
     infoLabel.setVisible(false);
     infoLabel.setEnabled(false);
     infoLabel.setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-
-    grid->addWidget(&storeTable, 1, 0, 2, 2);
-    storeTable.setRowCount(0);
-    storeTable.setColumnCount(3);
-    storeTable.setHorizontalHeaderItem(0, &idHeader);
-    storeTable.setHorizontalHeaderItem(1, &nameHeader);
-    storeTable.setHorizontalHeaderItem(2, &priceHeader);
-    storeTable.setVisible(false);
-    storeTable.setEnabled(false);
-    connect(&storeTable, &QTableWidget::cellActivated, [this](int row, int col) {
-        (void) col;
-        selectedItemLabel.setText(storeTable.item(row, 0)->text() + '\n' +
-                                  storeTable.item(row, 1)->text());
-        selectedItemLabel.setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-    });
-
-    grid->addWidget(&quantityText, 0, 0);
-    quantityText.setText("0");
-    quantityText.setVisible(false);
-    quantityText.setEnabled(false);
-
-    grid->addWidget(&buyButton, 0, 1);
-    buyButton.setVisible(false);
-    buyButton.setEnabled(false);
-    connect(&buyButton, SIGNAL(released()), this, SLOT(buyFunction()));
 
     grid->addWidget(&backButton, 0, 2);
     backButton.setVisible(false);
@@ -52,33 +20,11 @@ StoreMenu::StoreMenu(Game *game, QGridLayout *grid) :
 void StoreMenu::display() {
     this->pre_display();
 
-    QPalette p = storeTable.palette();
-    p.setBrush(QPalette::Base, Qt::transparent);
-    storeTable.setPalette(p);
-
     updateTable();
     updateInfo();
 
-    idHeader.setText(game->str.id);
-    nameHeader.setText(game->str.name);
-    priceHeader.setText(game->str.price);
-
-    selectedItemLabel.setVisible(true);
-    selectedItemLabel.setEnabled(true);
-
     infoLabel.setVisible(true);
     infoLabel.setEnabled(true);
-
-    storeTable.horizontalHeader()->setFont(game->font());
-    storeTable.setVisible(true);
-    storeTable.setEnabled(true);
-
-    quantityText.setVisible(true);
-    quantityText.setEnabled(true);
-
-    buyButton.setText(game->str.buy);
-    buyButton.setVisible(true);
-    buyButton.setEnabled(true);
 
     backButton.setText(game->str.back);
     backButton.setVisible(true);
@@ -87,39 +33,18 @@ void StoreMenu::display() {
     displayed = true;
 }
 
+
+void StoreMenu::addItem(const StoreMenu::Item &item, int &index) {
+
+}
+
 void StoreMenu::updateTable() {
     int index = 0;
-    static auto addDeal =
-        [this](const QString &name, const QString &description, qint64 price, int &index) {
-        storeTable.setRowCount(index + 1);
-        QTableWidgetItem *cell = storeTable.item(index, 0);
-        if (!cell) {
-            cell = new QTableWidgetItem;
-            storeTable.setItem(index, 0, cell);
-        }
-        cell->setText(name);
-        cell->setFlags(cell->flags() & (~Qt::ItemIsEditable));
-        cell = storeTable.item(index, 1);
-        if (!cell) {
-            cell = new QTableWidgetItem;
-            storeTable.setItem(index, 1, cell);
-        }
-        cell->setText(description);
-        cell->setFlags(cell->flags() & (~Qt::ItemIsEditable));
-        cell = storeTable.item(index, 2);
-        if (!cell) {
-            cell = new QTableWidgetItem;
-            storeTable.setItem(index, 2, cell);
-        }
-        cell->setText(QString::number(price));
-        cell->setFlags(cell->flags() & (~Qt::ItemIsEditable));
-        ++index;
-    };
     if (game->activeLocation == 0) {
-        addDeal("fish.pike", game->str.pike, 1000, index);
-        addDeal("net.basic", game->str.basicNet, 1000, index);
+        addItem(Item("fish.pike", 1000), index);
+        addItem(Item("net.basic", 1000), index);
     } else if (game->activeLocation == 1) {
-        addDeal("fish.ide", game->str.ide, 1100, index);
+        addItem(Item("fish.ide", 1100), index);
     }
 }
 
@@ -135,18 +60,14 @@ void StoreMenu::updateInfo() {
     game->gameMenu.getPopUpInventoryTable().updateInventoryTables();
 }
 
-void StoreMenu::buyFunction() {
-    if (storeTable.currentRow() == -1) {
-        QMessageBox::warning(game, game->str.fishingTime + ": " + game->str.warning, game->str.selectItemFromList);
-        return;
-    }
+void StoreMenu::buyFunction(int index) {
     bool ok;
-    int quantity = quantityText.text().toInt(&ok, 10);
+    int quantity = quantityText[index].text().toInt(&ok, 10);
     if (!ok) {
         QMessageBox::warning(game, game->str.fishingTime + ": " + game->str.warning, game->str.quantityShouldBeANumber);
         return;
     }
-    qint64 price = storeTable.item(storeTable.currentRow(), 2)->text().toLongLong();
+    qint64 price = item[index].price;
     qint64 sum = quantity * price;
     if (sum > game->users[game->activeUser].getCoins()) {
         QMessageBox::warning(game, game->str.fishingTime + ": " + game->str.warning, game->str.youDontHaveEnoughCoins);
@@ -156,13 +77,13 @@ void StoreMenu::buyFunction() {
         QMessageBox::warning(game, game->str.fishingTime + ": " + game->str.warning, game->str.quantityShouldBeMoreThanZero);
         return;
     }
-    game->users[game->activeUser].inventory.changeItem(storeTable.item(storeTable.currentRow(), 0)->text(), quantity);
+    game->users[game->activeUser].inventory.changeItem(item[index].id, quantity);
     game->users[game->activeUser].changeCoins(-sum);
     game->users[game->activeUser].inventory.updateStats(
-        storeTable.item(storeTable.currentRow(), 0)->text(),
+        item[index].id,
         "stats.bought", quantity, &game->str.stats
     );
-    game->logger.info("Bought " + QString::number(quantity) + " of " + QString::number(storeTable.currentRow()));
+    game->logger.info("Bought " + QString::number(quantity) + " of " + game->str.getItemName(item[index].id));
     updateInfo();
 }
 
@@ -174,20 +95,8 @@ void StoreMenu::backFunction() {
 void StoreMenu::hide() {
     this->pre_hide();
 
-    selectedItemLabel.setVisible(false);
-    selectedItemLabel.setEnabled(false);
-
     infoLabel.setVisible(false);
     infoLabel.setEnabled(false);
-
-    storeTable.setVisible(false);
-    storeTable.setEnabled(false);
-
-    quantityText.setVisible(false);
-    quantityText.setEnabled(false);
-
-    buyButton.setVisible(false);
-    buyButton.setEnabled(false);
 
     backButton.setVisible(false);
     backButton.setEnabled(false);
@@ -196,16 +105,5 @@ void StoreMenu::hide() {
 }
 
 StoreMenu::~StoreMenu() {
-    for (int i = 0; ; ++i) {
-        int cnt = 0;
-        for (int j = 0; j < 3; ++j) {
-            if (storeTable.item(i, j)) {
-                ++cnt;
-                delete storeTable.item(i, j);
-            }
-        }
-        if (!cnt) {
-            break;
-        }
-    }
+
 }
