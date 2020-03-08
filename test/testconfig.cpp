4e5ab980ff315fb0cba246c8620b7271b6170152
Arseniy Obolenskiy <gooddoog@student.su>
2020-03-08 18:37:05 +0300

    (#495) Convert C-string to QStringLiteral

--- test/testconfig.cpp
+++ test/testconfig.cpp
@@ -4,12 +4,12 @@
 void TestConfig::defaultConfigCheck() {
     Core core;
     QJsonObject jsonObj = core.cfg.serialize();
-    QCOMPARE(jsonObj["fishingtime_object"].toString(), "config");
+    QCOMPARE(jsonObj["fishingtime_object"].toString(), QStringLiteral("config"));
     QCOMPARE(jsonObj["autoSavePeriod"].toInt(), 3);
     QCOMPARE(jsonObj["colorTheme"].toInt(), 0);
     QCOMPARE(jsonObj["inventoryType"].toInt(), 0);
     QCOMPARE(jsonObj["language"].toInt(), 0);
     QCOMPARE(jsonObj["loggerLevel"].toInt(), 0);
     QCOMPARE(jsonObj["showBgImages"].toInt(), 1);
-    QCOMPARE(jsonObj["logFile"].toString(), "log.txt");
+    QCOMPARE(jsonObj["logFile"].toString(), QStringLiteral("log.txt"));
 }
